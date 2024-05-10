5df4a30c212d75a3a1305c373d364dbcacfe63b0
Thomas Tsai <tlinux.tsai@gmail.com>
2024-05-10 16:52:02 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'Kasm Technologies fork from thomas',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://thomas-tsai.github.io/kasm_registry/',
+    contactUrl: 'https://github.com/Thomas-Tsai/kasm_registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm_registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
